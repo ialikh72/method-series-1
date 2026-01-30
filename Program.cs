9272c9a6da52dd7778d5ea92c32402eb6a60cf7e
ialikh72 <ialikh.w1@gmail.com>
2026-01-30 20:04:41 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Gree
+{
+    static void Main()
+    {
+        Greet();
+    }
+    static void Greet()
+    {
+        string name;
+        Console.WriteLine("enter your name:");
+        name = Console.ReadLine();
+        Console.WriteLine(name+" you are amazing");
+    }
+}
+
